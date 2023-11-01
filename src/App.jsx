d0f14f171dd70b9c6e44b17fc2c1bcb452c5034e
joonaro <cupetus@gmail.com>
2023-11-01 13:16:09 +0200

    added cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import { useState } from 'react'
 import Header from './Header.jsx'
 import Card from './Card.jsx'
 import Footer from './Footer.jsx'
+import pic from './owl-50267_1280.jpg'
 function App() {
-  const [count, setCount] = useState(0)
+  const [animals, setAnimals] = useState([
+    { id: "1", name: "bear", },
+    { id: "2", name: "lion" }
+
+  ]);
 
   return (
     <>
       <Header />
       <main>
         <div className="cards">
-          <Card />
+          <Card name={animals[0].name} />
+          <Card name={animals[1].name} />
         </div>
       </main>
       <Footer />
@@ -18,4 +24,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,7 +1,13 @@
-const Card = () => {
+import pic from './owl-50267_1280.jpg'
+const Card = (props) => {
     return (
         <div className="card">
-            <p className="name">Animal</p>
+            <span class="close" onclick="">&times;</span>
+            <div className="imgHolder">
+
+            </div>
+
+            <p className="name">{props.name}</p>
         </div>
     )
 }
